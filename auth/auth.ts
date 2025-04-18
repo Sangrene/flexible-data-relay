cffaaa11aa8e2b51a690d549709a035c7b1efbf1
Hugo Laplace-Builhe <builhe@hotmail.fr>
2025-04-18 13:44:20 +0200

    add tests on authcore

--- auth/auth.ts
+++ auth/auth.ts
@@ -1,6 +1,7 @@
 import { err, ok, Result } from "neverthrow";
 import { Env } from "../env/loadEnv.ts";
 import { TenantCore } from "../tenants/tenant.core.ts";
+import { Tenant } from "../tenants/tenant.model.ts";
 import createJwtService from "./jwtService.ts";
 
 interface AuthServerProps {
@@ -36,19 +37,28 @@ export const createAuthCore = async ({ tenantCore, env }: AuthServerProps) => {
     return ok(token);
   };
 
-  const getTenantFromToken = async (token: string) => {
+  const getTenantFromToken = async (
+    token: string
+  ): Promise<
+    Result<Tenant, { error: "NO_TENANT_WITH_THIS_ID" | "BAD_CREDENTIALS" }>
+  > => {
     const { tenantId } = await jwtService.verifyJWT(token);
-    if (!tenantId) throw new WrongCredentialsError();
+    if (!tenantId) return err({ error: "BAD_CREDENTIALS" });
     const tenant = await tenantCore.getTenantById(tenantId as string);
-    return tenant;
+    if (!tenant) return err({ error: "NO_TENANT_WITH_THIS_ID" });
+    return ok(tenant);
   };
 
-  const generateAdminTokenFromSecret = async (secret: string) => {
+  const generateAdminTokenFromSecret = async (
+    secret: string
+  ): Promise<
+    Result<string, { error: "BAD_SECRET" | "NO_ADMIN_SECRET_DEFINED" }>
+  > => {
     const adminSecret = env.ADMIN_SECRET;
-    if (!adminSecret) return "Admin secret is not setup";
-    if (secret !== adminSecret) return "Secret not match";
+    if (!adminSecret) return err({ error: "NO_ADMIN_SECRET_DEFINED" });
+    if (secret !== adminSecret) return err({ error: "BAD_SECRET" });
     const token = await jwtService.createJWT({ admin: true });
-    return token;
+    return ok(token);
   };
 
   const getAdminFromToken = async (token: string) => {
